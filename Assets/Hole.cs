513198a58b5ffa12668691cf1f91093bd59a2355
0408petrus <petrus.marcellino15@students.usu.ac.id>
2022-10-31 00:04:48 +0700

    Update Tugas

---
+++ Assets/Hole.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    bool entered = false;
+    public bool Entered { get => entered; set => entered = value; }
+    private void OnTriggerEnter(Collider other)
+    {
+        entered = true;
+    }
+}
